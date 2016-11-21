d4906ce4dffa4b7b8ce7bcf84f232c3715135fd3
MisterTarock <victor_pb@hotmail.fr>
2016-11-21 18:11:02 +0100

    commet

--- Calculette/Calcu_main/Program.cs
+++ Calculette/Calcu_main/Program.cs
@@ -16,9 +16,11 @@ namespace Calcu_main
             // Chargement de l'assembly Computer.dll
             Assembly computer = Assembly.LoadFrom(@"..\..\..\Calcu_lib\bin\Debug\Calcu_lib.dll"); //@ allow us to put only one "\"
 
-            List<string> function; //To make a list with all the function possible //To init the variable
-            //We don't have to create a constructor in the MAin
-            function = new List<string>(); //To create my list
+            //To make a list with all the function possible 
+            //We init the variable with the first instruction
+            //We don't have to create a constructor in the Main
+            //The second instruction is to create my list
+            List<string> function; function = new List<string>();
 
             // Parcours de tout le contenu de l'assembly //to get all the informations
             foreach (Type t in computer.GetTypes())
@@ -103,72 +105,4 @@ namespace Calcu_main
             Console.ReadKey();
         }
 	}
-}
-
-
-/* while (true) //To compose our menu
-            {
-
-                double width = Console.WindowWidth; //To define the width of the window
-
-                for (int i = 0; i< ((width / 2) - 14); i++) //To make a beautiful title
-                {
-                    Console.Write("=");
-                }
-                Console.Write("Bienvenue dans Super Calculator 3.1 ");
-                for (int i = 0; i< ((width / 2) - 15); i++)
-                {
-                    Console.Write("=");
-                }
-
-                Console.WriteLine("What do you want to compute?");
-                Console.WriteLine("1) Addition");
-                Console.WriteLine("2) Multiplication");
-                Console.WriteLine("3) Sinus");
-                Console.WriteLine("4) Deriveé");
-                Console.WriteLine("5) Intégrale");
-                Console.WriteLine("6) Exit");
-
-                switch (Convert.ToString(Console.ReadLine()))  //To make the action of the 4 different possibilities or the default result
-                {
-                    case "1":
-                        Choice1.Init(students);
-
-                        break;
-
-                    case "2":
-                        Choice2.Init();
-                        break;
-                    case "3":
-                        Choice3.Init();
-                        break;
-                    case "4":
-                        EntityLister.Init();
-                        break;
-
-                    case "5":
-                        Console.Clear();
-                        Console.WriteLine("(T_T)");
-                        Console.WriteLine();
-                        Console.WriteLine("You know nothing! Jon Snow!");
-                        Console.WriteLine();
-                        Console.WriteLine("Press any key when you know");
-                        Console.ReadKey();
-                        Console.Clear();
-                        break;
-
-
-                    case "6":
-                        Environment.Exit(0);
-                        break;
-
-
-
-                    default:
-                        Console.WriteLine("Chose a valid choice please >:(");
-                        break;
-
-
-                }
-
-    */
+}   
